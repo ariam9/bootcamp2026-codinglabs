ac7d896ee49fb065f92e03e5cf72717f1a726919
Siddharth Maira <maira.siddharth@gmail.com>
2026-05-28 11:22:40 +0530

    assignment 3 done

--- assignment3-ispell-templates-concepts/spellcheck.cpp
+++ assignment3-ispell-templates-concepts/spellcheck.cpp
@@ -6,18 +6,50 @@
 #include <ranges>
 #include <set>
 #include <vector>
+#include <iterator>
+#include <concepts>
 
-template <typename Iterator, typename UnaryPred>
+template <std::input_iterator Iterator, std::indirect_unary_predicate<Iterator> UnaryPred>
 std::vector<Iterator> find_all(Iterator begin, Iterator end, UnaryPred pred);
 
 Corpus tokenize(std::string& source) {
   /* TODO: Implement this method */
-  return Corpus();
+  auto space_its = find_all(source.begin(), source.end(), ::isspace);
+
+  std::set<Token> tokens;
+
+  std::transform(
+    space_its.begin(), 
+    std::prev(space_its.end()), 
+    std::next(space_its.begin()), 
+    std::inserter(tokens, tokens.end()), 
+    [&source](const auto& start, const auto& end) {
+    return Token(source, start, end);
+  });
+
+  std::erase_if(tokens, [](const auto& token){
+    return token.content.empty();
+  });
+
+  return tokens;
 }
 
 std::set<Misspelling> spellcheck(const Corpus& source, const Dictionary& dictionary) {
   /* TODO: Implement this method */
-  return std::set<Misspelling>();
+  namespace rv = std::ranges::views;
+  auto view = source 
+    | rv::filter([&dictionary](const auto& token) {return !dictionary.contains(token.content);})
+    | rv::transform([&dictionary](const auto& token) {
+        auto view = dictionary | rv::filter([&dictionary, &token](const auto& word) {
+          return levenshtein(word, token.content) == 1; 
+        });
+
+        std::set<std::string> suggestions(view.begin(), view.end());
+        return Misspelling{token, suggestions};
+      })
+    | rv::filter([](const auto& misspelling) {return !misspelling.suggestions.empty();});
+
+  return std::set<Misspelling>(view.begin(), view.end());
 };
 
 /* Helper methods */
